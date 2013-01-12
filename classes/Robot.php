4133248475b335d972d1c8f053ec7eabb6fbfbeb
tysweezy <tyler@engajer.com>
2013-01-11 16:41:58 -0800

    Created a class called Robot
    After creating that class, I called it in index.php

---
+++ classes/Robot.php
@@ -0,0 +1,18 @@
+<?php
+/***
+  *Object-Oriented PHP Basics*
+  *Written By: Tyler Souza*
+  *URL: https://github.com/tysweezy/oop-php-basics*
+  *Description: This repo contains my learning process on Object-Oriented PHP*
+  *Feel free to edit my code and give me feedback/tips*
+***/
+
+class Robot {
+	function __construct() {
+		echo 'I created a class!!! Yay!!!';
+			
+	}
+		
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/***
+  *Object-Oriented PHP Basics*
+  *Written By: Tyler Souza*
+  *URL: https://github.com/tysweezy/oop-php-basics*
+  *Description: This repo contains my learning process on Object-Oriented PHP*
+  *Feel free to edit my code and give me feedback/tips*
+***/
+
+require_once 'classes/Robot.php';
+
+//calling the Robot class
+$robot = new Robot();
+//$robot->__construct();
+
+?>
